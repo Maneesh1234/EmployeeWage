7e7547c3a568bde1f723dc2aeb74f15d68b87384
Maneesh1234 <mk049076@gmail.com>
2024-01-05 15:56:18 +0530

    [Maneesh] Modify: Check whether employee is present or not

--- EmployeeWage/src/com/employee/Employee.java
+++ EmployeeWage/src/com/employee/Employee.java
@@ -3,8 +3,13 @@ package com.employee;
 public class Employee {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation Program");
-
+		int isPresent=(int)Math.floor(Math.random()*10) % 2;
+		if(isPresent==1) {
+			System.out.println("Employee is Present : "+isPresent);
+		}
+		else {
+			System.out.println("Employee is not Present : "+isPresent);
+		}
 	}
 
 }
