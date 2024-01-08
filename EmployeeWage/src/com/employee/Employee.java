f43122d7663ac1dc9f708553736ab142ce8be40a
Maneesh1234 <mk049076@gmail.com>
2024-01-08 17:37:59 +0530

    [Maneesh] Added: Employee Part time day wage calculation

--- EmployeeWage/src/com/employee/Employee.java
+++ EmployeeWage/src/com/employee/Employee.java
@@ -1,24 +1,38 @@
 package com.employee;
 
+import java.util.Random;
 
 public class Employee {
 
 	private static int WAGE_PER_HOUR=20;
 	private static int FULL_DAY_HOUR=8;
 	private static int FULL_DAY_WAGE=WAGE_PER_HOUR*FULL_DAY_HOUR;
+	private static int PART_TIME_HOUR=4;
+	private static int PART_TIME_WAGE=PART_TIME_HOUR*WAGE_PER_HOUR;
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Employee Wage Computation Program");
-		if(isPresent()) {
+		if(isEmpPresent()) {
 			System.out.println("Hi, Employee is Present ");
-			System.out.println("Full day wage is "+FULL_DAY_WAGE);
+			if (getEmpType().equals("part_time"))
+				System.out.println("Part time Day wage is "+PART_TIME_WAGE);
+			else if(getEmpType().equals("full_time"))
+				System.out.println("Full day wage is "+FULL_DAY_WAGE);
+			
 		}
 		else {
 			System.out.println("Employee is not Present ");
 		}
 	}
 	
-	public static boolean isPresent() {
+	public static String getEmpType() {
+		String [] s= {"full_time","part_time"};
+		int index=(int)(Math.random()*s.length);
+		return s[index];
+		
+	}
+	
+	public static boolean isEmpPresent() {
 		int is_present=(int)Math.floor(Math.random()*10) % 2;
 		if(is_present==1) {
 			return true;
