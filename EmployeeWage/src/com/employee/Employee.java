25f58d877af1123ef31006d6f8f9a0c1c872262a
Maneesh1234 <mk049076@gmail.com>
2024-01-08 15:52:40 +0530

    [Maneesh] Added: Check whether employee is present or not by using method

--- EmployeeWage/src/com/employee/Employee.java
+++ EmployeeWage/src/com/employee/Employee.java
@@ -5,7 +5,7 @@ public class Employee {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
 		if(isPresent()) {
-			System.out.println("Hi Employee is Present ");
+			System.out.println("Employee is Present ");
 		}
 		else {
 			System.out.println("Employee is not Present ");
