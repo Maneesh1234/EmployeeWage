f67d9c9636ae564fae94c17d2d44546c3f18ec4c
Maneesh1234 <mk049076@gmail.com>
2024-01-08 17:01:44 +0530

    [Maneesh] Added: Employee Full day wage calculation

--- EmployeeWage/src/com/employee/Employee.java
+++ EmployeeWage/src/com/employee/Employee.java
@@ -1,11 +1,17 @@
 package com.employee;
 
+
 public class Employee {
 
+	private static int WAGE_PER_HOUR=20;
+	private static int FULL_DAY_HOUR=8;
+	private static int FULL_DAY_WAGE=WAGE_PER_HOUR*FULL_DAY_HOUR;
 	public static void main(String[] args) {
+		
 		System.out.println("Welcome to Employee Wage Computation Program");
 		if(isPresent()) {
-			System.out.println("Employee is Present ");
+			System.out.println("Hi, Employee is Present ");
+			System.out.println("Full day wage is "+FULL_DAY_WAGE);
 		}
 		else {
 			System.out.println("Employee is not Present ");
